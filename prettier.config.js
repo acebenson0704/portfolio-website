ac95f0f0baa16b8557bea63d1a055a4979443637
acebenson0704 <acebenson0704@gmail.com>
2022-02-23 03:16:49 +0800

    basic template for next project

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'always',
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'crlf',
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'none'
+}
+
+module.exports = options
